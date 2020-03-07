4bf135e09b8a9fec2a4601c35779e9c6922ff802
createdTRYNA <m13542286803@163.com>
2020-03-07 15:44:33 +0800

    createdTRYNATest 3.7 15:44

--- src/main/java/com/demo/git/createdTRYNA.java
+++ src/main/java/com/demo/git/createdTRYNA.java
@@ -3,10 +3,14 @@ package com.demo.git;
 public class createdTRYNA {
     public static void main(String[] args) {
         System.out.println("say hello");
+
     }
     public void tryna4ever(){
         System.out.println("66666");
         System.out.println("66666");
         System.out.println("66666");
+    }
+    public void createdTRYNA(){
+        System.out.println("7777777");
     }
 }
