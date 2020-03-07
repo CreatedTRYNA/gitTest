70280c316292cf76b081d41a571ed9d39604b5ba
tryna <tryna4ever@foxmail.com>
2020-03-07 15:33:42 +0800

    tryna4everTest

--- src/main/java/com/demo/git/createdTRYNA.java
+++ src/main/java/com/demo/git/createdTRYNA.java
@@ -4,4 +4,7 @@ public class createdTRYNA {
     public static void main(String[] args) {
         System.out.println("say hello");
     }
+    public void tryna4ever(){
+        System.out.println("66666");
+    }
 }
